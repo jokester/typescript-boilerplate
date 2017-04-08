fd53541cf333e8c114442f1b04e6cc2e78612a05
Wang Guan <momocraft@gmail.com>
2017-04-09 03:56:08 +0900

    make use of $$webpack_dev

--- lib-ts/browser-entrypoint.ts
+++ lib-ts/browser-entrypoint.ts
@@ -1,1 +1,7 @@
-console.log("HEY");
+declare const $$webpack_dev: boolean;
+
+if ($$webpack_dev) {
+    document.write("hey, developer");
+} else {
+    document.write("hi, user");
+}
--- webpack/common.dev.js
+++ webpack/common.dev.js
@@ -2,7 +2,7 @@ const webpack = require('webpack');
 
 module.exports = {
   plugins: [
-    new webpack.DefinePlugin({ '$$webpack_dev': JSON.stringify(false)}),
+    new webpack.DefinePlugin({ '$$webpack_dev': JSON.stringify(true)}),
     // TODO adapt hot module new webpack.HotModuleReplacementPlugin(),
   ],
 
