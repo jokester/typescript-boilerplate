b04a52366a59f0a267291b7154e0bc278fe0ef9d
Wang Guan <momocraft@gmail.com>
2017-10-21 17:13:39 +0900

    split webpack HMR to new file

--- webpack/dev.js
+++ webpack/dev.js
@@ -5,6 +5,8 @@ const webpackMerge = require("webpack-merge");
 const path = require("path");
 const webpack = require("webpack");
 
+const usingHMR = !!process.argv.find(arg => /webpack-dev-server$/i.exec(arg));
+
 module.exports = webpackMerge([
   require("./common"),
   {
@@ -20,6 +22,7 @@ module.exports = webpackMerge([
     },
     plugins: [
       new webpack.DefinePlugin({ $$webpack_dev: JSON.stringify(true) }),
+      new webpack.DefinePlugin({ $$webpack_hmr: JSON.stringify(usingHMR) }),
       new webpack.NamedModulesPlugin(),
     ]
   }
