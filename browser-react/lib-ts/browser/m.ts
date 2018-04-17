10e5dd083ef00fc7871071b9c683e6967b6fd292
Wang Guan <momocraft@gmail.com>
2018-04-17 22:41:01 +0900

    recreate with create-react-app-typescript

--- browser-react/lib-ts/browser/m.ts
+++
@@ -1,9 +0,0 @@
-/**
- * This module is used to demostrate webpack's hot module replacement:
- * 1. Start dev-server with `npm run dev-server`
- * 2. Open http://localhost:9000/ and browser console
- * 3. Modify this file and see how it get hot-reloaded and accepted
- */
-export const v = 3;
-
-console.log("m.ts is being evaluted");
