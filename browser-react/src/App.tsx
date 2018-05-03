00d51f12316e7fee14fbcb3d1b0c5f7851baeb0e
Wang Guan <momocraft@gmail.com>
2018-05-04 00:06:51 +0900

    reduce duplicated code

--- browser-react/src/App.tsx
+++ browser-react/src/App.tsx
@@ -9,7 +9,7 @@ class App extends React.Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to React!</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
