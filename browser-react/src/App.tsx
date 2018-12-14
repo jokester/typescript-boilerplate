e80f39951bca612e71b0546f5a1ddee18a346d7e
Wang Guan <momocraft@gmail.com>
2018-12-14 22:10:44 +0900

    add node-sass and recreate yarn.lock

--- browser-react/src/App.tsx
+++ browser-react/src/App.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 class App extends Component {
   render() {
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload. ?25
+            Edit <code>src/App.js</code> and save to reload.
           </p>
           <a
             className="App-link"
