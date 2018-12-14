586267c2a6868b9ae9ea6898c00648f6a8e7abcb
Wang Guan <momocraft@gmail.com>
2018-12-14 22:00:20 +0900

    port from new create-react-app -created app

--- browser-react/src/App.tsx
+++ browser-react/src/App.tsx
@@ -1,19 +1,25 @@
-import * as React from 'react';
+import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
 
-import logoSvg from './logo.svg';
-
-class App extends React.Component {
-  public render() {
+class App extends Component {
+  render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logoSvg} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload. ?25
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
       </div>
     );
   }
--- browser-react/src/index.tsx
+++ browser-react/src/index.tsx
@@ -1,8 +1,8 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
-import registerServiceWorker from './registerServiceWorker';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
 function registerHMR() {
   type ModuleHMR = typeof module & {
@@ -23,6 +23,10 @@ function render() {
   );
 }
 
-registerServiceWorker();
 registerHMR();
 render();
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
