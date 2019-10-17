c4fccf1d16ca2083d7c901c68fb745e70a6a9daa
Wang Guan <momocraft@gmail.com>
2019-10-18 00:22:47 +0900

    lock on yarn install instead of smoke-test run

--- scripts/smoking-test.sh
+++ scripts/smoking-test.sh
@@ -6,14 +6,13 @@ cd $(dirname "$0")/..
 
 ensure-it-works () {
   pushd $d
-  yarn && yarn lint:fix && yarn build && yarn test && yarn test:coverage
+  yarn --mutex network && yarn lint:fix && yarn build && yarn test && yarn test:coverage
   popd
 }
 
 for d in node-lib web-nextjs browser-react browser-preact ; do
   echo "testing in '$d' . see 'TEST-$d.log' for result"
-  ensure-it-works $d &> "TEST-$d.log"
-  wait
+  ensure-it-works $d &> "TEST-$d.log" &
 done
 
 wait
