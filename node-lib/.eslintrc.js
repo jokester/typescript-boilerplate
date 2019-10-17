b753e01fd487f06fafd97b9e15d222e983c16d6a
Wang Guan <momocraft@gmail.com>
2019-10-17 23:52:26 +0900

    sync eslint configs

--- node-lib/.eslintrc.js
+++ node-lib/.eslintrc.js
@@ -1,10 +1,13 @@
-// following https://dev.to/robertcoopercode/using-eslint-and-prettier-in-a-typescript-project-53jb
 module.exports =  {
-  parser:  '@typescript-eslint/parser',  // Specifies the ESLint parser
+  parser:  '@typescript-eslint/parser',
+  plugins: [ '@typescript-eslint' ],
   extends:  [
-    'prettier/@typescript-eslint',  // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
-    'plugin:@typescript-eslint/recommended',  // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-    'plugin:prettier/recommended',  // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+    // "eslint:recommended", // not enabling: it reports many TS idioms as error
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    // "plugin:@typescript-eslint/recommended-requiring-type-checking", // cant find for unknonw reason
+    'plugin:prettier/recommended',
+    'prettier/@typescript-eslint',
   ],
   parserOptions:  {
     ecmaVersion:  2018,  // Allows for the parsing of modern ECMAScript features
@@ -13,15 +16,16 @@ module.exports =  {
   rules:  {
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
     // e.g. "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/indent": ["warn", 2],
     "@typescript-eslint/no-var-requires": 2,
     "@typescript-eslint/no-unused-vars": 0,
     "@typescript-eslint/explicit-member-accessibility": 0,
     "@typescript-eslint/no-non-null-assertion": 1,
-    "@typescript-eslint/no-use-before-define": 1,
+    "@typescript-eslint/no-use-before-define": 0,
     "@typescript-eslint/no-empty-interface": 0,
-    "@typescript-eslint/no-parameter-properties": 1,
-    "@typescript-eslint/no-namespace": 1,
+    "@typescript-eslint/no-parameter-properties": 0,
+    "@typescript-eslint/no-namespace": 0,
+    "@typescript-eslint/no-empty-function": 0,
+    "@typescript-eslint/no-explicit-any": 0,
   },
   settings:  {
     react:  {
