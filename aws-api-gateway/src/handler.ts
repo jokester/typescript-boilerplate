0c891fefa02a69d47b2055ea18eef3898482d5a5
Wang Guan <momocraft@gmail.com>
2020-02-09 13:22:27 +0900

    create aws-api-gateway fork

---
+++ aws-api-gateway/src/handler.ts
@@ -0,0 +1,18 @@
+import {APIGatewayEvent, APIGatewayEventRequestContext} from "aws-lambda"
+
+export const hello = async (event: APIGatewayEvent, context: unknown) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Go Serverless v1.0! Your function executed successfully!',
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
