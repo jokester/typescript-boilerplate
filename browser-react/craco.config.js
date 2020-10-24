d2fbfb21a666d9ab220df994fbb1e152f52b747e
Wang Guan <momocraft@gmail.com>
2020-10-24 15:33:06 +0900

    support preact in browser-react variant

--- browser-react/craco.config.js
+++ browser-react/craco.config.js
@@ -1,5 +1,13 @@
-module.exports = async (args) => {
-  console.log('craco args', args);
-  return args;
-}
-
+module.exports = async ({ env }) => {
+  if (/* change to true to bundle preact instead of react */ 0) {
+    return {
+      webpack: {
+        alias: {
+          react: 'preact/compat',
+          'react-dom': 'preact/compat',
+        },
+      },
+    };
+  }
+  return {};
+};
--- browser-react/src/index.tsx
+++ browser-react/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import reportWebVitals from './reportWebVitals';
 
 function registerHMR() {
   type ModuleHMR = typeof module & {
@@ -18,13 +18,18 @@ function registerHMR() {
 }
 
 function render() {
-  ReactDOM.render(<App />, document.getElementById('root') as HTMLElement);
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root'),
+  );
 }
 
 registerHMR();
 render();
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals(console.debug);
