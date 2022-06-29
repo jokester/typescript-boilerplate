140c136a3c2f40390a8fa7049d271728f74fd758
Wang Guan <momocraft@gmail.com>
2022-06-29 13:03:46 +0900

    follow nextjs cautions

--- web-nextjs/pages/_document.tsx
+++ web-nextjs/pages/_document.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import Document, { Html, Main, NextScript, Head } from 'next/document';
 
-export default class CustomDocument extends Document {
-  render(): React.ReactElement {
-    return (
-      <Html>
-        <Head />
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+const defaultMetaTags = [
+  <link
+    rel="stylesheet"
+    key="css-tailwindcss"
+    href="https://cdn.jsdelivr.net/npm/tailwindcss@2.0.2/dist/tailwind.min.css"
+    integrity="sha256-KwBcfPYYUP4pXG0aiIA8nTSuAqRzRWdtoHQktxvMVf4="
+    crossOrigin="anonymous"
+  />,
+] as const;
+export default function CustomDocument(): React.ReactElement {
+  return (
+    <Html>
+      <Head>{defaultMetaTags} </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
--- web-nextjs/src/components/meta/default-meta.tsx
+++ web-nextjs/src/components/meta/default-meta.tsx
@@ -1,22 +1,9 @@
+import React from 'react';
 import Head from 'next/head';
 
-import React from 'react';
-
+/**
+ * @deprecated effectively empty now
+ */
 export const DefaultMeta: React.FC = () => {
-  return (
-    <Head>
-      <link
-        rel="stylesheet"
-        key="css-tailwindcss"
-        href="https://cdn.jsdelivr.net/npm/tailwindcss@2.0.2/dist/tailwind.min.css"
-        integrity="sha256-KwBcfPYYUP4pXG0aiIA8nTSuAqRzRWdtoHQktxvMVf4="
-        crossOrigin="anonymous"
-      />
-      <meta
-        key="meta-viewport"
-        name="viewport"
-        content="width=device-width, initial-scale=1,maximum-scale=1.5,minimum-scale=1"
-      />
-    </Head>
-  );
+  return <Head />;
 };
