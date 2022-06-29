5cbcc9752db35214a0862c1199c94ecbb3dd286d
Wang Guan <momocraft@gmail.com>
2022-06-30 01:29:05 +0900

    remove experimental code

--- web-nextjs/pages/fast-event-viewer.tsx
+++
@@ -1,6 +0,0 @@
-import { ViewerDemo } from '../src/components/pref-test/emitted-viewer';
-import React from 'react';
-
-const ViewerPage: React.FC = () => <ViewerDemo />;
-
-export default ViewerPage;
