f2495c2421e58ddd3022d36b2dcdcc052aa82174
Wang Guan <momocraft@gmail.com>
2024-02-13 01:47:00 +0900

    cherry-pick me: prettify next/jest.config

--- next/jest.config.js
+++ next/jest.config.js
@@ -1,5 +1,5 @@
 /**
- * @type {import('ts-jest').InitialOptionsTsJest}
+ * @type {import('jest').Config}
  */
 const customJestConfig = {
   roots: ['pages', 'app', 'src', 'shared', 'server'],
@@ -8,23 +8,26 @@ const customJestConfig = {
   collectCoverageFrom: ['**/*.(ts|tsx)', '!build/', '!**/node_modules', '!/coverage'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
   coverageReporters: ['json', 'lcov', 'text', 'html'],
-  // moduleNameMapper: {
-  //   '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-  //     '<rootDir>/src/test/mocks/resolves-to-path.json',
-  //   '\\.(css|less|scss|sass)$': '<rootDir>/src/test/mocks/resolves-to-path.json',
-  // },
-  // transform: {
-  //   '^.+\\.(js|jsx|ts|tsx)$': [
-  //     'ts-jest',
-  //     {
-  //       isolatedModules: true,
-  //       tsconfig: {
-  //         jsx: 'react',
-  //       },
-  //     },
-  //   ],
-  // },
-  // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+};
+
+const unusedConfig = {
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/src/test/mocks/resolves-to-path.json',
+    '\\.(css|less|scss|sass)$': '<rootDir>/src/test/mocks/resolves-to-path.json',
+  },
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': [
+      'ts-jest',
+      {
+        isolatedModules: true,
+        tsconfig: {
+          jsx: 'react',
+        },
+      },
+    ],
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
 };
 
 const nextJest = require('next/jest');
