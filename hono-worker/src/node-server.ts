46a6bd152ad1693b15ec4b0f2011ccdca79199c8
Wang Guan <momocraft@gmail.com>
2024-08-24 18:56:52 +0900

    hono: add node entrypoint

---
+++ hono-worker/src/node-server.ts
@@ -0,0 +1,17 @@
+import { Hono } from 'hono';
+import { serve } from '@hono/node-server'
+
+const app = new Hono();
+app.all('*', async (c) => {
+    const reqUrl = c.req.raw.url
+    const headers = c.req.header()
+    const reqBody = await c.req.json().catch(e => 'not json')
+    console.log({ reqUrl, headers, reqBody })
+    return c.text("It works!")
+})
+const server = serve(app, (info) => {
+    console.log(`Listening on http://localhost:${info.port}`) // Listening on http://localhost:3000
+})
+
+
+
